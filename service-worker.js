1e076851498584dff31af36fdaf0ec19267a5d42
noagrp <lihwlee11@gmail.com>
2026-09-20 23:48:35 +0800

    Bump imageNgine application cache

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='imagengine-v2';
-const APP_SHELL=['./','./index.html','./engine.html','./enhancements.js?v=2','./manifest.webmanifest?v=2','./favicon-32x32.png?v=2','./icon-192.png?v=2','./icon-512.png?v=2','./apple-touch-icon.png?v=2'];
+const CACHE_NAME='imagengine-v3';
+const APP_SHELL=['./','./index.html','./engine.html','./enhancements.js?v=3','./manifest.webmanifest?v=2','./favicon-32x32.png?v=2','./icon-192.png?v=2','./icon-512.png?v=2','./apple-touch-icon.png?v=2'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{const r=e.request;if(r.method!=='GET')return;const u=new URL(r.url);if(r.mode==='navigate'){e.respondWith(fetch(r).then(res=>{const copy=res.clone();caches.open(CACHE_NAME).then(c=>c.put(r,copy));return res;}).catch(()=>caches.match(r).then(hit=>hit||caches.match('./index.html'))));return;}if(u.origin===self.location.origin){e.respondWith(caches.match(r).then(hit=>hit||fetch(r).then(res=>{const copy=res.clone();caches.open(CACHE_NAME).then(c=>c.put(r,copy));return res;})));}});
